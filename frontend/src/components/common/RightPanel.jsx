07535f845ac700789b98413d41886012b6322179
umarkhan905 <umairzahidkhang@gmail.com>
2024-07-01 09:36:44 +0500

    3-Suggested Users Fetched

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
 import { RightPanelSkeleton } from "../";
+import { useQuery } from "@tanstack/react-query";
 const RightPanel = () => {
-  const isLoading = false;
+  const { data: suggestedUsers, isLoading } = useQuery({
+    queryKey: ["suggestedUsers"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/user/suggested");
+        const data = await res.json();
+        if (!res.ok)
+          throw new Error(data.error || "Failed to fetch suggested users");
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+  if (suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>;
 
   return (
     <div className="hidden lg:block my-4 mx-2">
@@ -19,7 +33,7 @@ const RightPanel = () => {
             </>
           )}
           {!isLoading &&
-            USERS_FOR_RIGHT_PANEL?.map((user) => (
+            suggestedUsers?.map((user) => (
               <Link
                 to={`/profile/${user.username}`}
                 className="flex items-center justify-between gap-4"
